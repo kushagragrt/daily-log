78ba73e51e6d1890094682f12be985243e646299
kushagragrt <kushagra.1300@gmail.com>
2026-06-28 22:59:09 +0530

    Fix Supabase anon key: replace publishable key with standard JWT anon key

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 // key is designed to be used in browser code.
 // ============================================================
 window.SUPABASE_URL = "https://tkvctzkzkrclozabfaie.supabase.co";
-window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_EqHxhF8MiIqzop80ZeDENA_DQasim8z";
+window.SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdmN0emt6a3JjbG96YWJmYWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTk1NzYsImV4cCI6MjA5ODIzNTU3Nn0.1Ewopz2ZPwhQMcPRq92kr2AWELezK5-zvhNaBiiFM24";
